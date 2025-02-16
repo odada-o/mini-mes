7f6c240be079e239d2940ca766e7a38d0a172b0f
odada-o <eehd80yul@gmail.com>
2025-02-16 22:12:25 +0900

    todo

---
+++ client/src/features/todo/interfaces/PageParam.ts
@@ -0,0 +1,6 @@
+export interface PageParam {
+
+    page: number,
+    size: number
+
+}
---
+++ client/src/features/todo/interfaces/PageResponse.ts
@@ -0,0 +1,16 @@
+export interface PageResponse<T> {
+
+    totalCount: number,
+    prevPage: number,
+    nextPage: number,
+    totalPage: number,
+    current: number
+
+    prev:boolean
+    next:boolean
+
+    pageNumList:[number]
+
+    dtoList:[T]
+
+}
